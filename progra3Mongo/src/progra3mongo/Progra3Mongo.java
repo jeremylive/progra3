f22d20519c7b787768ced15043b31fc041c3c56d
Jeremy Live <jeremylive@live.com>
2017-11-26 10:40:18 -0600

    avance#2

--- progra3Mongo/src/progra3mongo/Progra3Mongo.java
+++ progra3Mongo/src/progra3mongo/Progra3Mongo.java
@@ -1,5 +1,6 @@
 package progra3mongo;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.Mongo;
@@ -37,5 +38,47 @@ public class Progra3Mongo
             JOptionPane.showMessageDialog(null, ex.getMessage());
         }
     }
+    
+    
+    
+    //CRUD RESUMEN
+    public void createResumen(int numero_partido, String codEq1, String codEq2, 
+                                String txtResumen, String video1, String video2)
+    {
+        BasicDBObject document = new BasicDBObject();
+        document.put("numero_partido",numero_partido);
+        document.put("codigo_equi1","'"+codEq1+"'");
+        document.put("codigo_equi2","'"+codEq2+"'");
+        document.put("txt_resumen","'"+txtResumen+"'");
+        document.put("video1","'"+video1+"'");
+        document.put("video2","'"+video2+"'");
+        tabla.insert(document);
+        
+    }
+    
+    public void updateResumen(int numero_partido, String codEq1, String codEq2, 
+                                String txtResumen, String video1, String video2)
+    {
+        BasicDBObject document = new BasicDBObject();
+        document.put("numero_partido",numero_partido);
+        document.put("codigo_equi1","'"+codEq1+"'");
+        document.put("codigo_equi2","'"+codEq2+"'");
+        document.put("txt_resumen","'"+txtResumen+"'");
+        document.put("video1","'"+video1+"'");
+        document.put("video2","'"+video2+"'");
+        tabla.insert(document);
+    }    
+    
+    public void readResumen()
+    {
+        
+    }
+
+    public void deleteResumen(int numero_partido)
+    {
+        
+    }
+    
+    //CRUD 
     
 }
