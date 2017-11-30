2ab0128f9a5064d1f80e7a5b1875ae71dbd6a11e
Jurgenn <moralesjur@gmail.com>
2017-11-29 22:30:51 -0600

    Merge #2
    interfazReadAficionados + arreglo funciones

--- progra3Mongo/src/progra3mongo/interfazReadAficionados.java
+++ progra3Mongo/src/progra3mongo/interfazReadAficionados.java
@@ -1,7 +1,15 @@
 package progra3mongo;
 
 import com.mongodb.DBObject;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import static sun.applet.AppletResourceLoader.getImage;
 
 /**
  *
@@ -10,8 +18,45 @@ import java.util.ArrayList;
 public class interfazReadAficionados extends javax.swing.JFrame 
 {
 
+    public class Imagen extends javax.swing.JPanel 
+    {
+        String url;
+        
+        public Imagen(String url1) 
+        {
+            this.setSize(300, 400); //se selecciona el tamaño del panel
+            this.url = url1;
+        }
+
+        //Se crea un método cuyo parámetro debe ser un objeto Graphics
+
+        public void paint(Graphics grafico) 
+        {
+           
+            try {
+                 Dimension height = getSize();
+            
+                JLabel etiqueta = new JLabel();
+                Image img=getImage(new URL(url));
+                etiqueta.setIcon(new ImageIcon(img));
+                //repaint();
+                
+                grafico.drawImage(img, 0, 0, height.width, height.height, null);
+
+                setOpaque(false);
+                super.paintComponent(grafico);
+
+               } catch (MalformedURLException ex) {
+                // TODO Auto-generated catch block
+                ex.printStackTrace();
+               }
+            
+        }
+    }
+    
+    
     //Variables globales
-    private ArrayList<Integer> listaAficionado = new ArrayList<>();  //lista de numeros de los resumen
+    private ArrayList<String> listaAficionado = new ArrayList<>();  //lista de numeros de los resumen
     private int contador = -1;                                      //cont
     private Controlador control;                                    //controlador
     private String urlVideo1, urlVideo2;                            //Videos urls
@@ -42,7 +87,7 @@ public class interfazReadAficionados extends javax.swing.JFrame
         //Verifica si existe por lo menos un resumen
         if (listaAficionado.size() != 0) {
             contador++;
-            DBObject tupla = control.readAficionado(listaAficionado.get(contador));
+            DBObject tupla = control.readAficionado(Integer.parseInt(listaAficionado.get(contador)));
             cargarReadAficionado((String) tupla.get("codigo_aficionado"),
                     (String) tupla.get("contraseña"),
                     (String) tupla.get("foto"),
@@ -55,6 +100,8 @@ public class interfazReadAficionados extends javax.swing.JFrame
             }
         }
     }
+    
+ 
     
     
     
@@ -74,6 +121,8 @@ public class interfazReadAficionados extends javax.swing.JFrame
         siguiente = new javax.swing.JButton();
         Cargar = new javax.swing.JButton();
         fotoAfi = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jFoto = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -121,6 +170,24 @@ public class interfazReadAficionados extends javax.swing.JFrame
 
         fotoAfi.setEnabled(false);
 
+        jButton1.setText("ver");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jFotoLayout = new javax.swing.GroupLayout(jFoto);
+        jFoto.setLayout(jFotoLayout);
+        jFotoLayout.setHorizontalGroup(
+            jFotoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 404, Short.MAX_VALUE)
+        );
+        jFotoLayout.setVerticalGroup(
+            jFotoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -128,11 +195,11 @@ public class interfazReadAficionados extends javax.swing.JFrame
             .addGroup(layout.createSequentialGroup()
                 .addGap(274, 274, 274)
                 .addComponent(jLabel1)
-                .addGap(0, 0, Short.MAX_VALUE))
+                .addGap(0, 340, Short.MAX_VALUE))
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(238, 238, 238)
+                        .addGap(63, 63, 63)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(jLabel8)
@@ -149,15 +216,18 @@ public class interfazReadAficionados extends javax.swing.JFrame
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(jLabel2)
                                 .addGap(18, 18, 18)
-                                .addComponent(codAfi, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                                .addComponent(codAfi, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton1))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(175, 175, 175)
                         .addComponent(Cargar, javax.swing.GroupLayout.PREFERRED_SIZE, 83, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(34, 34, 34)
                         .addComponent(siguiente)
                         .addGap(35, 35, 35)
                         .addComponent(salir, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(218, Short.MAX_VALUE))
+                .addGap(26, 26, 26)
+                .addComponent(jFoto, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -165,27 +235,32 @@ public class interfazReadAficionados extends javax.swing.JFrame
                 .addContainerGap()
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(codAfi, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel2))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(contraAfi, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel3))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel4)
-                    .addComponent(fotoAfi, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(correoAfi, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel8))
-                .addGap(31, 31, 31)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(salir, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(siguiente, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(Cargar, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(33, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(codAfi, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel2))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(contraAfi, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel3))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel4)
+                            .addComponent(fotoAfi, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jButton1))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(correoAfi, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel8))
+                        .addGap(31, 31, 31)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(salir, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(siguiente, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(Cargar, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(0, 178, Short.MAX_VALUE))
+                    .addComponent(jFoto, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         pack();
@@ -215,12 +290,23 @@ public class interfazReadAficionados extends javax.swing.JFrame
         
     }//GEN-LAST:event_codAfiActionPerformed
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        
+        Imagen Imagen = new Imagen(fotoAfi.getText());
+        jFoto.add(Imagen);
+        jFoto.repaint();
+        
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton Cargar;
     private javax.swing.JTextField codAfi;
     private javax.swing.JTextField contraAfi;
     private javax.swing.JTextField correoAfi;
     private javax.swing.JTextField fotoAfi;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JPanel jFoto;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
